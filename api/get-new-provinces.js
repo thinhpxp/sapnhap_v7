147b27c00bbdef9597076ee90d57f477b812b49a
thinhpxp <thinhpxp@gmail.com>
2025-09-09 12:34:38 +0700

    schema API

--- api/get-new-provinces.js
+++ api/get-new-provinces.js
@@ -1,12 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY, {
+  db: { schema: 'api' }
+});
 export default async function handler(request, response) {
-  try {
-    const { data, error } = await supabase.rpc('get_unique_new_provinces');
-    if (error) throw error;
-    response.status(200).json(data);
-  } catch (error) {
-    console.error('Lỗi API get-new-provinces:', error);
-    response.status(500).json({ error: 'Lỗi máy chủ.' });
-  }
+      try {
+        const { data, error } = await supabase.rpc('get_unique_new_provinces');
+        if (error) throw error;
+        response.status(200).json(data);
+      } catch (error) {
+        console.error('Lỗi API get-new-provinces:', error);
+        response.status(500).json({ error: 'Lỗi máy chủ.' });
+      }
 }
