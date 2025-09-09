56e32ba3b14ea223c361ad9913e7b58503262138
thinhpxp <thinhpxp@gmail.com>
2025-09-09 18:10:11 +0700

    nhắn chi tiết cho tui

--- locales/vi.js
+++ locales/vi.js
@@ -80,7 +80,7 @@ window.translations = {
   "agency_cong_an_xa": "Công an xã",
 
    // ...
-  "feedbackDescription": "Bạn phát hiện có sự thiếu sót? Hãy nhắn cho mình. Phiên bản thứ 4 đang được phát triển để tăng độ chính xác và tốc độ tra cứu.",
+  "feedbackDescription": "Bạn phát hiện có sự thiếu sót? Hãy nhắn thật chi tiết cho mình",
   "feedbackPlaceholder": "Nhập góp ý của bạn...",
   "feedbackSendBtn": "Gửi",
   "feedbackSuccess": "Cám ơn, các phản hồi sẽ được mình xử lý vào mỗi cuối tuần",
