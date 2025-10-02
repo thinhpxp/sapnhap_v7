326690f19585657f1b300231e5663aac148fa37c
thinhpxp <thinhpxp@gmail.com>
2025-10-02 14:26:13 +0700

    api feedback.js

--- script.js
+++ script.js
@@ -697,7 +697,7 @@ document.addEventListener('DOMContentLoaded', () => {
         feedbackSendBtn.textContent = '...';
 
         try {
-            const response = await fetch('/api/submit-feedback', {
+            const response = await fetch('/api/feedback', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
